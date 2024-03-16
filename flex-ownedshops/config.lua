3a6c98d0f5397ba786f776c0185efc56288274ac
Lucasmillen1 <145896347+Lucasmillen1@users.noreply.github.com>
2024-03-16 04:32:31 -0400

    Update config.lua
    added 
    
    bank = 'true',  -- True = money gets deposited into store account, false = society account

--- flex-ownedshops/config.lua
+++ flex-ownedshops/config.lua
@@ -1,82 +1,86 @@
 Config = {}
-Config.Debug = true
-Config.inventorylink = 'qb-inventory/html/images/' --Path of your inventory images
-
-Config.Banking = 'qb' -- fd for fd_banking / qb for banking
-
-Config.Management = 'new' -- 'new' for qb-banking handling management, 'old' for qb-management
 
 
--- Still working on getting none job/gang shops to work, please only use shop with job name.
--- Use ped models, having issues with vending machines endlessly spawning 
+Config.Debug = false -- Enable debug mode for logging (true/false)
+
+Config.inventorylink = 'qb-inventory/html/images/' -- Path for inventory images, change to whatever your pathway is
+
+Config.Banking = 'qb' -- Banking system used: 'fd' for fd_banking, 'qb' for qb-banking
+
+Config.Management = 'new' -- Management system used: 'new' for qb-banking management, 'old' for qb-management
+
+
+-- List of shops with their properties
 
 Config.Shops = {
     [1] = {
-        shopname = 'testing', -- Name of shop
-        shopprice = 1000, -- Price of this shop. When it is gang or job owned no need for price
-        manageloc = vector4(-321.62, 6193.39, 31.48, 249), -- Place where you manage the shop
-        buyloc = vector4(-314.82, 6200.14, 31.51, 339), -- Location to buy from shop
-        target = false, -- leave this false for now! If true it will spawn a vendingmachine at buyloc to target on
+        shopname = 'testing',  -- Name of the shop
+        bank = 'true',  -- True = money gets deposited into store account, false = society account
+        shopprice = 1000,  -- Price of the shop (gang or job-owned shops don't change price)
+        manageloc = vector4(-321.62, 6193.39, 31.48, 249),  -- Location to manage the shop
+        buyloc = vector4(-314.82, 6200.14, 31.51, 339),  -- Location to buy items from the shop
+        target = false,  -- Spawn a vending machine at buyloc to target (leave false until update)
         isjob = {
-            name = 'police', -- false to disable job
-            everyone = true, -- true if all ranks can access
-            dutyloc = vector4(-205.95, -627.39, 48.22, 248.61), -- nil to disable
+            name = 'police',  -- Job associated with the shop, false if gang true
+            everyone = true,  -- All ranks can access if true
+            dutyloc = nil,  -- Location for duty access (nil to disable)
         },
         isgang = {
-            name = false, -- false to disable gang
-            everyone = false, -- true if all ranks can access
+            name = false,  -- Gang associated with the shop (false to disable gang)
+            everyone = false,  -- All ranks can access if true
         },
         machine = {
-            model = nil, -- Modelname or nil
-            offset = { -- Ofset to the zone
+            model = nil,  -- Vending machine model or nil (leave nil, this is in testing!!)
+            offset = {  -- Offset to the zone
                 x = 0.0,
                 y = 0.0,
                 z = 0.0,
             }
         },
         ped = {
-            model = 'ig_mp_agent14', -- Name of the ped or nil to disable
-            scenario = 'WORLD_HUMAN_CLIPBOARD', -- Scenario the ped will be dpoing
+            model = 'ig_mp_agent14',  -- Ped model or nil to disable
+            scenario = 'WORLD_HUMAN_CLIPBOARD',  -- Scenario for the ped
         },
         boxzone = {
-            width = 2.0, -- Width of the target / box zone
-            depth = 2.0, -- Depth of the target / box zone
-            minZ = 2.0, -- Minz of the target zone (No need if not target)
-            maxZ = 1.0, -- Maxz of the target zone (No need if not target)
+            width = 2.0,  -- Width of the target/box zone
+            depth = 2.0,  -- Depth of the target/box zone
+            minZ = 2.0,  -- Min Z of the target zone (no need if not target)
+            maxZ = 1.0,  -- Max Z of the target zone (no need if not target)
         },
     },
     [2] = {
-        shopname = '247shop2', -- Name of shop
-        shopprice = 50000, -- Price of this shop. When it is gang or job owned no need for price
-        manageloc = vector4(-305.25, 6211.41, 31.48, 214), -- Place where you manage the shop
-        buyloc = vector4(-310.05, 6207.30, 31.44, 220), -- Location to buy from shop
-        target = false, -- leave this false for now! If true it will spawn a vendingmachine at buyloc to target on
+        shopname = '247shop',  -- Name of the shop
+        bank = 'true',  -- True = money gets deposited into store account, false = society account
+        shopprice = 50000,  -- Price of the shop (gang or job-owned shops don't need a price)
+        manageloc = vector4(-305.25, 6211.41, 31.48, 214),  -- Location to manage the shop
+        buyloc = vector4(-310.05, 6207.30, 31.44, 220),  -- Location to buy items from the shop
+        target = false,  -- Spawn a vending machine at buyloc to target (leave false until update)
         isjob = {
-            name = 'false', -- false to disable job (not working right now)
-            everyone = true, -- true if all ranks can access
-            dutyloc = nil, -- nil to disable
+            name = 'realestate',  -- Job associated with the shop, false if gang true
+            everyone = true,  -- All ranks can access if true
+            dutyloc = vector4(-205.95, -627.39, 48.22, 248.61),  -- Location for duty access (nil to disable)
         },
         isgang = {
-            name = false, -- false to disable gang
-            everyone = false, -- true if all ranks can access
+            name = false,  -- Gang associated with the shop (false to disable gang)
+            everyone = false,  -- All ranks can access if true
         },
         machine = {
-            model = nil, -- Modelname or nil
-            offset = { -- Ofset to the zone
+            model = nil,  -- Vending machine model or nil
+            offset = {  -- Offset to the zone
                 x = 0.0,
                 y = 0.0,
                 z = 0.0,
             }
         },
         ped = {
-            model = 'ig_mp_agent14', -- Name of the ped or nil to disable
-            scenario = 'WORLD_HUMAN_CLIPBOARD', -- Scenario the ped will be dpoing
+            model = 'ig_mp_agent14',  -- Vending machine model or nil (leave nil, this is in testing!!)
+            scenario = 'WORLD_HUMAN_CLIPBOARD',  -- Scenario for the ped
         },
         boxzone = {
-            width = 2.0, -- Width of the target / box zone
-            depth = 2.0, -- Depth of the target / box zone
-            minZ = 2.0, -- Minz of the target zone (No need if not target)
-            maxZ = 1.0, -- Maxz of the target zone (No need if not target)
+            width = 2.0,  -- Width of the target/box zone
+            depth = 2.0,  -- Depth of the target/box zone
+            minZ = 2.0,  -- Min Z of the target zone (no need if not target)
+            maxZ = 1.0,  -- Max Z of the target zone (no need if not target)
         },
     },
 }
