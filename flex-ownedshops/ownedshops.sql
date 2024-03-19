20ebd81c40f4f83090b2a8d6735d95a1a5ea712e
Lucas Millen <145896347+Lucasmillen1@users.noreply.github.com>
2024-03-19 19:39:28 -0400

    Update ownedshops.sql

--- flex-ownedshops/ownedshops.sql
+++ flex-ownedshops/ownedshops.sql
@@ -12,16 +12,19 @@ CREATE TABLE `ownedshops` (
   `id` int(11) NOT NULL,
   `shopname` varchar(255) NOT NULL,
   `owner` varchar(50) NOT NULL,
+  `owned` tinyint(1) NOT NULL DEFAULT 0,
   `stock` longtext DEFAULT NULL,
   `bank_account_balance` decimal(10,2) NOT NULL DEFAULT 0.00,
   `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `bank` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+
 ALTER TABLE `ownedshops`
   ADD KEY `owner` (`owner`),
   ADD KEY `id` (`id`),
   ADD KEY `last_updated` (`last_updated`);
+
 
 ALTER TABLE `ownedshops`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
