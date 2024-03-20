5fa8977ff2b4e5aee0269c08adceb4d29978c804
Lucas Millen <145896347+Lucasmillen1@users.noreply.github.com>
2024-03-19 22:45:49 -0400

    Update ownedshops.sql

--- flex-ownedshops/ownedshops.sql
+++ flex-ownedshops/ownedshops.sql
@@ -1,35 +1,13 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
-
-CREATE TABLE `ownedshops` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `ownedshops` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `shopname` varchar(255) NOT NULL,
   `owner` varchar(50) NOT NULL,
   `owned` tinyint(1) NOT NULL DEFAULT 0,
   `stock` longtext DEFAULT NULL,
   `bank_account_balance` decimal(10,2) NOT NULL DEFAULT 0.00,
   `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `bank` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
-ALTER TABLE `ownedshops`
-  ADD KEY `owner` (`owner`),
-  ADD KEY `id` (`id`),
-  ADD KEY `last_updated` (`last_updated`);
-
-
-ALTER TABLE `ownedshops`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  `bank` tinyint(1) NOT NULL DEFAULT 0,
+  KEY `owner` (`owner`),
+  KEY `id` (`id`),
+  KEY `last_updated` (`last_updated`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
